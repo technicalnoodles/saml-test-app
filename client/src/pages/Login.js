4e0887641112b313dc5ec056aa2280f12e76b4b9
galica9999 <galica9999@gmail.com>
2023-01-23 18:35:50 -0500

    Update Login.js

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,16 +1,8 @@
+import { useSelector } from 'react-redux';
+
 const Login = () => {
-    return (
-        <div className='row'>
-        <div className="col-md-3">
-        <div className="form-group base-margin-bottom">
-            <div className="form-group__text">
-                <input id="input-type-email" type="email" value="dadams@cisco.com"/>
-                <label for="input-type-email">Email</label>
-            </div>
-        </div>
-    </div>
-    </div>
-    )
-}
+  const loggedIn = useSelector((state) => state.auth);
+  return <h2>{loggedIn ? 'Go to events' : 'Please login'}</h2>;
+};
 
 export default Login;
