f54dc8d407704e21390d74e7c6d6eae0ed08369d
galica9999 <galica9999@gmail.com>
2023-01-23 12:52:09 -0500

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 import './styles/cui-standard.min.css';
+
+const store = createStore(() => {}, {}, applyMiddleware());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
